42036b74615bb334cb5ce30d6d3e86b50a36d04c
fananchong <fananchong@126.com>
2018-12-21 13:14:51 +0800

    add proto

--- make.sh
+++ make.sh
@@ -2,19 +2,19 @@
 
 set -ex
 
-export SRC_DIR=$PWD
-export BIN_DIR=$PWD/bin
-export SERVICE_DIR=$PWD/services/
-export FLAG_RACE=-race
-export GOBIN=$BIN_DIR
+SRC_DIR=$PWD
+BIN_DIR=$PWD/bin
+SERVICE_DIR=$PWD/services/
+FLAGS=-race
+GOBIN=$BIN_DIR
 
 go vet ./...
 cd $SERVICE_DIR
 for plugin_name in `ls -l | grep '^d' | awk '{print $9}'`; do
-    go build $FLAG_RACE -buildmode=plugin -o $BIN_DIR/$plugin_name.so ./$plugin_name;
+    go build $FLAGS -buildmode=plugin -o $BIN_DIR/$plugin_name.so ./$plugin_name;
 done
 cd $SRC_DIR
-go install $FLAG_RACE .
+go install $FLAGS .
 
 case $1 in
     "docker") docker build -t go-xserver . ;;
@@ -23,10 +23,3 @@ case $1 in
         ;;
     ?);;
 esac
-
-set +ex
-export SRC_DIR=
-export BIN_DIR=
-export SERVICE_DIR=
-export FLAG_RACE=
-export GOBIN=
