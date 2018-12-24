db60fb1102773c04712b945156644f2e116120f8
fananchong <fananchong@126.com>
2018-12-24 22:28:47 +0800

    up

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/fananchong/go-xserver
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/fananchong/glog v0.0.0-20181217161741-c97cfee433d9
 	github.com/fananchong/go-redis-orm.v2 v0.0.0-20181223152030-e0011659cc35
 	github.com/fananchong/gotcp v0.0.0-20181223035403-4b5dac1478d0
--- internal/node_normal/session.go
+++ internal/node_normal/session.go
@@ -9,18 +9,23 @@ import (
 
 // Session : 网络会话类
 type Session struct {
-	gotcp.Session
+	*gotcp.Session
 }
 
 // Start : 开始连接 Mgr Server
 func (sess *Session) Start() bool {
+	sess.connectMgrServer()
+	return true
+}
+
+func (sess *Session) connectMgrServer() {
+	sess.Session = &gotcp.Session{}
 TRY_AGAIN:
 	addr, port := getMgrInfoByBlock()
 	if sess.Connect(fmt.Sprintf("%s:%d", addr, port), sess) == false {
 		time.Sleep(1 * time.Second)
 		goto TRY_AGAIN
 	}
-	return true
 }
 
 // OnRecv : 接收到网络数据包，被触发
@@ -32,4 +37,5 @@ func (sess *Session) OnRecv(data []byte, flag byte) {
 
 // OnClose : 断开连接，被触发
 func (sess *Session) OnClose() {
+	go sess.connectMgrServer()
 }
