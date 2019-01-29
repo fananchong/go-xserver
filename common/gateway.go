56a584ac686f9959efb80ec01e3a38ad43ca6e82
fananchong <fananchong@126.com>
2019-01-29 12:23:58 +0800

    更新文档

--- common/gateway.go
+++ common/gateway.go
@@ -10,7 +10,8 @@ package common
 //    4. 框架层会接管几乎所有的网关的功能（网关会很纯粹，主要就是消息转发）
 
 // 网关模块逻辑层负责的工作：
-//    1. 自定义加解密算法
+//    1. 自定义客户端交互协议（如使用 TCP 、 HTTP ； 如使用 proto 消息、 struct 消息 等等）
+//    2. 自定义加解密算法
 
 // 经过网关的消息规则，请参见：[规范-消息号.md](go-xserver/doc/规范-消息号.md)
 
@@ -20,8 +21,13 @@ type FuncTypeEncode func(data []byte) []byte
 // FuncTypeDecode : 数据解密函数声明
 type FuncTypeDecode func(data []byte) []byte
 
+// FuncTypeSendToClient : 发送客户端数据函数声明
+type FuncTypeSendToClient func(data []byte) bool
+
 // IGateway : 网关模块接口
 type IGateway interface {
-	RegisterEncodeFunc(f FuncTypeEncode)
-	RegisterDecodeFunc(f FuncTypeDecode)
+	OnRecvFromClient(data []byte) (done bool)    // 可自定义客户端交互协议。data 格式需转化为框架层可理解的格式。done 为 true ，表示框架层接管处理该消息
+	RegisterSendToClient(f FuncTypeSendToClient) // 可自定义客户端交互协议
+	RegisterEncodeFunc(f FuncTypeEncode)         // 可自定义加解密算法
+	RegisterDecodeFunc(f FuncTypeDecode)         // 可自定义加解密算法
 }
