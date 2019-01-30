41cd97a59ef28c94535daab5ad327d9d5750cf82
fananchong <fananchong@126.com>
2019-01-30 11:39:20 +0800

    更新脚本 make.sh

--- make.sh
+++ make.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+
+if [[ $1 == "stop" ]]; then
+    pkill go-xserver
+    ps -ux | grep go-xserver
+    exit 0 
+fi
+
+
 CUR_DIR=$PWD
 SRC_DIR=$PWD
 BIN_DIR=$PWD/bin
@@ -15,7 +23,8 @@ cd $SRC_DIR
 go vet ./...
 
 cd $SERVICE_DIR
-for plugin_name in `ls -l | grep '^d' | awk '{print $9}' | grep -v 'internal'`; do
+plugins=`ls -l | grep '^d' | awk '{print $9}' | grep -v 'internal'`
+for plugin_name in $plugins; do
     go build $FLAGS -buildmode=plugin -o $BIN_DIR/$plugin_name.so ./$plugin_name;
 done
 cd $SRC_DIR
@@ -25,8 +34,18 @@ case $1 in
     "docker") docker build -t go-xserver . ;;
     "start")
         cd $BIN_DIR
+        mkdir -p $BIN_DIR/logs
+        mkdir -p $BIN_DIR/logs.back
+        mv $BIN_DIR/logs/* $BIN_DIR/logs.back/
+        for plugin_name in $plugins; do
+            nohup ./go-xserver --app $plugin_name > /dev/null 2>&1 &
+        done
+        ps -ux | grep go-xserver
         ;;
     ?);;
 esac
 
 cd $CUR_DIR
+
+exit 0
+
